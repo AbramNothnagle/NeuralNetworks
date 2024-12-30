3e613748cd874e9f0cc9b8ca0f98be39ee36162b
Abram <abramnothnagle@gmail.com>
2024-12-30 15:37:12 -0500

    removed garbage comments

--- experiment1_NumFeatures3FunctionLogic.py
+++ experiment1_NumFeatures3FunctionLogic.py
@@ -25,11 +25,6 @@ y = data['y'].values
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Standardize the features
-#scaler = StandardScaler()
-#X_train = scaler.fit_transform(X_train)
-#X_test = scaler.transform(X_test)
-
 # Convert data to PyTorch tensors
 X_train = torch.tensor(X_train, dtype=torch.float32)
 y_train = torch.tensor(y_train, dtype=torch.float32).unsqueeze(1)
